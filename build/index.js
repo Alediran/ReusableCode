a713a628dfaa5123cba7404bd34f92b7c6fdb156
Alediran <55855331+Alediran@users.noreply.github.com>
2020-05-21 13:23:51 -0300

    Final Version

--- build/index.js
+++ build/index.js
@@ -1,16 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
-var manUnitedWins = 0;
-for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
-        manUnitedWins++;
-    }
-}
-console.log("Man United won " + manUnitedWins + " games");
+var CsvFileReader_1 = require("./CsvFileReader");
+var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+var WinsAnalysis_1 = require("./Analyzers/WinsAnalysis");
+var Summary_1 = require("./Summary");
+var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './Analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
 
-let manUnitedWins = 0;
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
-    manUnitedWins++;
-  }
-}
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
 
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
